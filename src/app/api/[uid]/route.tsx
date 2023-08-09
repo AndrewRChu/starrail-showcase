265dd435588210dd09a16d2fd0f9cb9c98f683df
Andrew Chu <88413150+AndrewRChu@users.noreply.github.com>
2023-08-08 20:12:32 -0400

    Update route.tsx

--- src/app/api/[uid]/route.tsx
+++ src/app/api/[uid]/route.tsx
@@ -4,6 +4,8 @@ export async function GET(
     request: Request,
     { params }: { params: { uid: string } }
 ) {
+    const { searchParams } = new URL(request.url);
+    const uid = searchParams.get("uid");
     const res = await fetch(
         `https://api.mihomo.me/sr_info_parsed/${params.uid}?lang=en`
     );
